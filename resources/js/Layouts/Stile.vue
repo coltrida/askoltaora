f2d711031e896cc3bbbede326afad0c001edff2e
coltrida <coltrida@gmail.com>
2023-12-29 23:30:46 +0100

    inserito layout

---
+++ resources/js/Layouts/Stile.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-toolbar
+            dark
+            prominent
+            color="primary"
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Vuetify</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-menu>
+                <template v-slot:activator="{ props }">
+                    <v-btn icon v-bind="props">
+                        <v-avatar>
+                            <v-img
+                                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                alt="John"
+                            ></v-img>
+                        </v-avatar>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item>
+                        <v-list-item-title>Logout</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-toolbar>
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Stile"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,15 +5,36 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Stile from './Layouts/Stile.vue';
+
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+    },
+    components,
+    directives
+})
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    /*resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/!**!/!*.vue')),*/
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || Stile
+        return page
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .use(ZiggyVue)
             .mount(el);
     },
